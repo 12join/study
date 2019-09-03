1ec55cf6a761f3d0a1e568fa1382efb7e28b4e58
zhouwei <zhouwei@.cn>
2019-09-03 22:40:49 +0800

    提交代码

---
+++ test-fastdfs/src/main/java/com/xuecheng/test/fastdfs/TestFastDFSApplication.java
@@ -0,0 +1,17 @@
+package com.xuecheng.test.fastdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ **/
+
+@SpringBootApplication
+public class TestFastDFSApplication {
+	//启动类
+    public static void main(String[] args) {
+        SpringApplication.run(TestFastDFSApplication.class,args);
+
+    }
+}
